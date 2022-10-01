d32c2af3807502656abf494a450492f0a617a8ff
kellwyn131 <astarossa@hotmail.com>
2022-10-01 17:24:09 -0300

    First Commit

---
+++ src/main/java/br/com/uniamerica/npju/entity/Controle.java
@@ -0,0 +1,9 @@
+package br.com.uniamerica.npju.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_controles",schema = "NPJU")
+public class Controle extends AbstractEntity {
+}
